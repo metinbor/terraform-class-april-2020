e127bcbad2f3e85ae9d60496c0ecda247115c05a
EC2 Default User <ec2-user@ip-172-31-6-249.eu-west-2.compute.internal>
2020-06-21 09:37:55 +0000

    regions

--- class4/backend.tf
+++ class4/backend.tf
@@ -1,11 +1,7 @@
 terraform {
-  required_version = "0.11.14"
-  backend "s3" {
-    bucket = "terraform-state-april-class-metin"
-    key    = "path/to/my/key"
-    region = "us-east-1"
+backend "s3" {
+bucket = "terraform-state-april-class-metin"
+key = "jenkins/us-east-1/tools/sau_paulo/jenkins.tfstate"
+region = "us-east-1"
   }
 }
-
-
-#backend you can't interpolite; this has to be made manually
